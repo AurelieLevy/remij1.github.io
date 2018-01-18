091fe8a901292bc46679337037142550f90d647b
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-01-18 23:16:45 +0100

    t

--- app/modules/home/homeCtrl.js
+++ app/modules/home/homeCtrl.js
@@ -77,7 +77,7 @@
 					if (data.status === 0) {
 						vm.error = 'Could not get my data';
 					} else if (data.status === 1) {
-						vm.userData = data.response.data;
+						$scope.userData = data.response.data;
 					}
 
 					$scope.$apply();
