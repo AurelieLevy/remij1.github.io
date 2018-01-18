6bae89ca025e243e7cbfae9951799b04fcbf345a
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-01-18 21:31:05 +0100

    debugging shopService

--- app/modules/shop/shopService.js
+++ app/modules/shop/shopService.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 	'use strict';
 
 	/**
@@ -9,74 +9,80 @@
 	 * Service of the app
 	 */
 
-  	angular
+	angular
 		.module('shop')
 		.factory('shopService', shopService);
-		// Inject your dependencies as .$inject = ['$http', 'someSevide'];
-		// function Name ($http, someSevide) {...}
+	// Inject your dependencies as .$inject = ['$http', 'someSevide'];
+	// function Name ($http, someSevide) {...}
 
-		shopService.$inject = ['$http'];
+	shopService.$inject = ['$http'];
 
-		function shopService ($http) {
-			return {
-				getBuyablePhotos: getBuyablePhotos,
-				postCode: postCode,
-			};
+	function shopService($http) {
+		return {
+			getBuyablePhotos: getBuyablePhotos,
+			postCode: postCode,
+		};
 
-			function getBuyablePhotos(urlString, token) {
-				return new Promise((resolve) => {
-					console.log(`Fetching ${urlString}...`);
-	
-					// Simple GET request example:
-					$http({
-						method: 'GET',
-						url: urlString,
-						headers: {
-							'x-access-token': token.access_token
-						}
-					}).then(function successCallback(response) {
-							// this callback will be called asynchronously
-							// when the response is available
-							resolve({
-								status: 1, 
-								response,
-							});
-						}, function errorCallback(response) {
-							// called asynchronously if an error occurs
-							// or server returns response with an error status.
-							resolve({
-								status: 0, 
-								response,
-							});
-						});
+		function getBuyablePhotos(urlString, token) {
+			return new Promise((resolve) => {
+				console.log(`Fetching ${urlString}...`);
+
+				// Simple GET request example:
+				$http({
+					method: 'GET',
+					url: urlString,
+					headers: {
+						'x-access-token': token.access_token
+					}
+				}).then(function successCallback(response) {
+					// this callback will be called asynchronously
+					// when the response is available
+					resolve({
+						status: 1,
+						response,
+					});
+				}, function errorCallback(response) {
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
+					resolve({
+						status: 0,
+						response,
+					});
 				});
-			}
-
-			function postCode(urlString) {
-				return new Promise((resolve) => {
-					console.log(`Fetching ${urlString}...`);
-	
-					// Simple GET request example:
-					$http({
-						method: 'POST',
-						url: urlString
-					}).then(function successCallback(response) {
-							// this callback will be called asynchronously
-							// when the response is available
-							resolve({
-								status: 1, 
-								response,
-							});
-						}, function errorCallback(response) {
-							// called asynchronously if an error occurs
-							// or server returns response with an error status.
-							resolve({
-								status: 0, 
-								response,
-							});
-						});
-				});
-			}
+			});
 		}
+
+		function postCode(urlString) {
+			console.log("Posting code to aureda");
+			return new Promise((resolve) => {
+				console.log(`Fetching ${urlString}...`);
+
+				let body = {
+					"code" : code
+				}
+
+				// Simple GET request example:
+				$http({
+					method: 'POST',
+					url: urlString,
+					data: JSON.stringify(body)
+				}).then(function successCallback(response) {
+						// this callback will be called asynchronously
+						// when the response is available
+						resolve({
+							status: 1, 
+							response,
+						});
+					}, function errorCallback(response) {
+						// called asynchronously if an error occurs
+						// or server returns response with an error status.
+						resolve({
+							status: 0, 
+							response,
+						});
+					});
+			});
+		}
+	}
 
 })();
