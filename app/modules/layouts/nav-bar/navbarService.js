2a597f2d259936d38bf96e487acf682016155770
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-01-23 21:09:54 +0100

    navbarTest

--- app/modules/layouts/nav-bar/navbarService.js
+++ app/modules/layouts/nav-bar/navbarService.js
@@ -1,40 +1,39 @@
-'use strict';
-
-(function () {
+(function() {
+	'use strict';
 
 	/**
-	* @ngdoc function
-	* @name app.service:menuService
-	* @description
-	* # menuService
-	* Service of the app
-	*/
+	 * @ngdoc function
+	 * @name app.service:menuService
+	 * @description
+	 * # menuService
+	 * Service of the app
+	 */
 
-	angular
+  	angular
 		.module('toDoProject')
 		.factory('MenuService', Menu);
+		// Inject your dependencies as .$inject = ['$http', 'someSevide'];
+		// function Name ($http, someSevide) {...}
 
-	// Inject your dependencies as .$inject = ['$http', '$anotherDependency'];
-	// function Name ($http, $anotherDependency) {...}
+		Menu.$inject = ['$http'];
 
-	Menu.$inject = ['$http'];
+		function Menu ($http) {
 
-	function Menu($http) {
-		// Sample code.
+			var menu = [
+				
+					{
+						link: 'shop',
+							name: 'Shop'
+					},
+			    
+		  	];
 
-		var menu = [
-			{
-				link: '.',
-				name: 'This is a Placeholder menu. It disappears when the first module has been created.'
-			}
-		];
+			return {
+				listMenu: function () {
+					return menu;
+				}
+		  	}
 
-		return {
-			listMenu: function () {
-				return menu;
-			}
 		}
-
-	}
 
 })();
